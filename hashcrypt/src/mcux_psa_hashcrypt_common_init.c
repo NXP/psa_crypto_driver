93c63b2c7c005b378dc82e5987627ffd947b4793
Patrik Nemeth (nxf98608) <patrik.nemeth@nxp.com>
2026-01-21 14:58:14 +0100

    [MCUX-85453][hashcrypt] Fix missing mutex init and deinit for RNG
    Hashcrypt uses separate mutexes for RNG operations that were never
    initialized. This was causing mutex operations to be called on NULL
    mutexes.
    
    Signed-off-by: Patrik Nemeth

--- hashcrypt/src/mcux_psa_hashcrypt_common_init.c
+++ hashcrypt/src/mcux_psa_hashcrypt_common_init.c
@@ -21,8 +21,20 @@ psa_status_t hashcrypt_common_init(void)
         return status;
     }
 
+    if (mcux_mutex_init(&rng_hwcrypto_mutex) != 0) {
+        return PSA_ERROR_SERVICE_FAILURE;
+    }
+
+    if (mcux_mutex_lock(&rng_hwcrypto_mutex) != 0) {
+        return PSA_ERROR_SERVICE_FAILURE;
+    }
+
     /* Init RNG peripheral */
     status = mcux_psa_hashcrypt_entropy_init(&rng_hwcrypto_mutex);
+
+    if (mcux_mutex_unlock(&rng_hwcrypto_mutex) != 0) {
+        return PSA_ERROR_SERVICE_FAILURE;
+    }
 
     return status;
 }
@@ -31,8 +43,18 @@ psa_status_t hashcrypt_common_free(void)
 {
     status_t hashcrypt_status = CRYPTO_DeinitHardware_Hashcrypt();
 
+    if (mcux_mutex_lock(&rng_hwcrypto_mutex) != 0) {
+        return PSA_ERROR_SERVICE_FAILURE;
+    }
+
     /* Deinit RNG peripheral */
     mcux_psa_hashcrypt_entropy_deinit();
+
+    if (mcux_mutex_unlock(&rng_hwcrypto_mutex) != 0) {
+        return PSA_ERROR_SERVICE_FAILURE;
+    }
+
+    (void)mcux_mutex_free(&rng_hwcrypto_mutex);
 
     return hashcrypt_to_psa_status(hashcrypt_status);
 }
