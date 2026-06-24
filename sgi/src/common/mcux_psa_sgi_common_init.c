790758e633eca23ec5791dff46167403b1b91cdb
Eliot Menoret <eliot.menoret@nxp.com>
2026-06-24 10:32:57 +0200

    [MCSE-1651][PSA][SGI] Add unsafe variants of crypto hardware init/deinit functions ofr lp support

--- sgi/src/common/mcux_psa_sgi_common_init.c
+++ sgi/src/common/mcux_psa_sgi_common_init.c
@@ -45,17 +45,38 @@ status_t CRYPTO_InitHardware(void)
         return kStatus_Fail;
     }
 
-    /* Enable SGI and related HW */
-    result = SGI_PowerDownWakeupInit(SGI0);
-    if (result != kStatus_Success) {
-        goto exit;
+    result = CRYPTO_InitHardwareUnsafe();
+
+    if (mcux_mutex_unlock(&sgi_hwcrypto_mutex) != 0) {
+        return kStatus_Fail;
     }
 
-    g_isCryptoHWInitialized = true;
+    return result;
+}
 
-exit:
-    if (mcux_mutex_unlock(&sgi_hwcrypto_mutex) != 0) {
-        result = kStatus_Fail;
+/*!
+ * @brief Application init for Crypto blocks.
+ *
+ * Unsafe version of CRYPTO_InitHardware that does not take mutexs.
+ *
+ * @warning This function does not acquire any mutex locks and is not thread-safe.
+ *          If called incorrectly by an application (e.g., from multiple threads
+ *          concurrently or without proper external synchronization), this may
+ *          cause race conditions, undefined behavior, or hardware access issues.
+ *          Use CRYPTO_InitHardware() for thread-safe initialization.
+ */
+status_t CRYPTO_InitHardwareUnsafe(void)
+{
+    status_t result = kStatus_Fail;
+
+    if (g_isCryptoHWInitialized == true) {
+        return kStatus_Success;
+    }
+
+    /* Enable SGI and related HW */
+    result = SGI_PowerDownWakeupInit(SGI0);
+    if (result == kStatus_Success) {
+        g_isCryptoHWInitialized = true;
     }
 
     return result;
@@ -93,4 +114,26 @@ status_t CRYPTO_DeinitHardware(void)
     }
 
     return result;
+}
+
+/*!
+ * @brief Application Deinit for Crypto blocks.
+ *
+ * Unsafe version of CRYPTO_DeinitHardware that does not take mutexs and do not close handles.
+ *
+ * @warning This function does not acquire any mutex locks and is not thread-safe.
+ *          If called incorrectly by an application (e.g., from multiple threads
+ *          concurrently or without proper external synchronization), this may
+ *          cause race conditions, undefined behavior, or hardware access issues.
+ *          Use CRYPTO_DeinitHardware() for thread-safe de-initialization.
+ */
+status_t CRYPTO_DeinitHardwareUnsafe(void)
+{
+    if (g_isCryptoHWInitialized == false) {
+        return kStatus_Success;
+    }
+
+    g_isCryptoHWInitialized = false;
+
+    return kStatus_Success;
 }
