5496909cd76313329a1600b73ceb72a3fcab01f8
Waqar Tahir <waqar.tahir@nxp.com>
2026-06-25 18:23:37 +0200

    [IOTDL-2882] cmake updates to support tfm v2.3 builds with HW acceleration in zephyr-downstream
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

--- cmake/tfm.cmake
+++ cmake/tfm.cmake
@@ -10,7 +10,7 @@ if(CONFIG_BUILD_WITH_TFM)
   # Common paths
   #---------------------------------------------------------------------------
   set(TFM_SOURCE_DIR            ${CMAKE_SOURCE_DIR})
-  set(NXP_MODULE_CRYPTO_PATH    ${MBEDCRYPTO_PATH}/../)
+  set(NXP_MODULE_CRYPTO_PATH    ${TF_PSA_CRYPTO_PATH}/../)
 
   # Common PSA / mbedTLS compile definitions
   # The builtin key for initial attestation is only included if TF-M is 
@@ -19,28 +19,33 @@ if(CONFIG_BUILD_WITH_TFM)
     MBEDTLS_MCUX_USE_TRNG_AS_ENTROPY_SEED
     PSA_CRYPTO_DRIVER_TFM_BUILTIN_KEY_LOADER
     $<$<BOOL:TFM_PARTITION_INITIAL_ATTESTATION>:TFM_PARTITION_INITIAL_ATTESTATION>
-    MBEDTLS_CONFIG_FILE="${TFM_MBEDCRYPTO_CONFIG_PATH}"
-    MBEDTLS_PSA_CRYPTO_CONFIG_FILE="${TFM_MBEDCRYPTO_PSA_CRYPTO_CONFIG_PATH}"
-    $<$<BOOL:${TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH}>:
-      MBEDTLS_USER_CONFIG_FILE="${TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH}">
+    TF_PSA_CRYPTO_CONFIG_FILE="${TFM_TF_PSA_CRYPTO_CONFIG_PATH}"
+    $<$<BOOL:${MBEDTLS_PSA_CRYPTO_PLATFORM_FILE}>:MBEDTLS_PSA_CRYPTO_PLATFORM_FILE="${MBEDTLS_PSA_CRYPTO_PLATFORM_FILE}">
+    $<$<BOOL:${TFM_TF_PSA_CRYPTO_PLATFORM_EXTRA_CONFIG_PATH}>:
+      TF_PSA_CRYPTO_USER_CONFIG_FILE="${TFM_TF_PSA_CRYPTO_PLATFORM_EXTRA_CONFIG_PATH}">
   )
-    
+
   #---------------------------------------------------------------------------
   # SGI PKC PSA crypto driver
   #---------------------------------------------------------------------------
   if(CONFIG_PSA_CRYPTO_DRIVER_SGI_PKC)
 
     add_library(McuxPsaCryptoDriverSgiPkc)
-    set(NXP_MODULE_CRYPTO_PATH ${MBEDCRYPTO_PATH}/../)
+    set(NXP_MODULE_CRYPTO_PATH ${TF_PSA_CRYPTO_PATH}/../)
     set(NXP_MODULE_SGI_PKC_PATH ${NXP_MODULE_CRYPTO_PATH}sgi_pkc)
     set(NXP_MODULE_PSA_CRYPTO_DRIVER_PATH ${NXP_MODULE_CRYPTO_PATH}psa_crypto_driver)
 
     target_include_directories(McuxPsaCryptoDriverSgiPkc
-      PRIVATE
-        # mbedTLS / PSA
-        ${NXP_MODULE_CRYPTO_PATH}/mbedtls-3.6/include
-        ${NXP_MODULE_CRYPTO_PATH}/mbedtls-3.6/include/psa
-        ${NXP_MODULE_CRYPTO_PATH}/mbedtls-3.6/library
+        PRIVATE
+        # psa/crypto.h and psa/crypto_types.h
+        ${NXP_MODULE_CRYPTO_PATH}/mbedtls/include
+        ${NXP_MODULE_CRYPTO_PATH}/mbedtls/include/psa
+        # for psa driver wrapper
+        ${NXP_MODULE_CRYPTO_PATH}/mbedtls/library
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/core
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/drivers/builtin/src
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/drivers/pqcp/src
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/dispatch
     )
 
     target_compile_definitions(McuxPsaCryptoDriverSgiPkc
@@ -78,8 +83,26 @@ if(CONFIG_BUILD_WITH_TFM)
   if(CONFIG_MCUX_PSA_CRYPTO_DRIVER_ELS_PKC)
 
     add_library(McuxPsaCryptoDriverElsPkc)
-    set(NXP_MODULE_CRYPTO_PATH ${MBEDCRYPTO_PATH}/../)
+    set(NXP_MODULE_CRYPTO_PATH ${TF_PSA_CRYPTO_PATH}/../)
     set(NXP_MODULE_ELS_PKC_PATH ${NXP_MODULE_CRYPTO_PATH}/els_pkc)
+
+    target_include_directories(McuxPsaCryptoDriverElsPkc
+        PRIVATE
+        # psa/crypto.h and psa/crypto_types.h
+        ${NXP_MODULE_CRYPTO_PATH}/mbedtls/include
+        ${NXP_MODULE_CRYPTO_PATH}/mbedtls/include/psa
+        # for psa driver wrapper
+        ${NXP_MODULE_CRYPTO_PATH}/mbedtls/library
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/include
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/include/psa
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/drivers/builtin/include
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/core
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/drivers/builtin/src
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/drivers/pqcp/src
+        ${NXP_MODULE_CRYPTO_PATH}/tf-psa-crypto/dispatch
+        # cmsis required to build els_pkc
+        ${NXP_MODULE_ELS_PKC_PATH}/src/platforms/rw61x/cmsis_cortex_m_5_5_1/inc
+    )
 
     # Common TF-M include paths required, as the MBEDTLS_PSA_CRYPTO_CONFIG_FILE includes the config_tfm.h
     set(TFM_COMMON_INCLUDES
